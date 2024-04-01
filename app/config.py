df59a64ebc33b6f1ca74cc325f905350ef7ee846
Collins Miles <165222751+milescollins862@users.noreply.github.com>
2024-04-01 08:44:18 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,12 +7,12 @@ import logging
 def load_configurations(app):
     load_dotenv()
     app.config["ACCESS_TOKEN"] = os.getenv("ACCESS_TOKEN")
-    app.config["YOUR_PHONE_NUMBER"] = os.getenv("YOUR_PHONE_NUMBER")
+    app.config["YOUR_PHONE_NUMBER"] = os.getenv("254716308170")
     app.config["APP_ID"] = os.getenv("APP_ID")
     app.config["APP_SECRET"] = os.getenv("APP_SECRET")
     app.config["RECIPIENT_WAID"] = os.getenv("RECIPIENT_WAID")
-    app.config["VERSION"] = os.getenv("VERSION")
-    app.config["PHONE_NUMBER_ID"] = os.getenv("PHONE_NUMBER_ID")
+    app.config["VERSION"] = os.getenv("android")
+    app.config["PHONE_NUMBER_ID"] = os.getenv("41992683")
     app.config["VERIFY_TOKEN"] = os.getenv("VERIFY_TOKEN")
 
 
