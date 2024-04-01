74e37fa414702d46d7c059e0b424bf4486950892
milescollins862 <165222751+milescollins862@users.noreply.github.com>
2024-04-01 05:09:56 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == "__main__": miles 
     logging.info("Flask app started")
     app.run(host="0.0.0.0", port=8000)
+phone number:254716308170
